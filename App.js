e4f78e37f18be22ba68e049169ff1fbfa0fe2cbe
heshanu <heshanu97@gmail.com>
2023-04-02 20:27:48 +0530

    first comment

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import StartGameScreen from "./scrrens/StartGameScreen";
+export default function App() {
+  return (
+    <>
+      <View style={styles.container}>
+        <Text>Hello World!!!!</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View><StartGameScreen /></View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ scrrens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import React,{useState} from "react";
+import { View,StyleSheet, } from "react-native-web";
+
+const StartGameScreen = () => {
+    return (
+        <View style={styles.screen}>
+            <Text>The Game Screen</Text>
+        </View>
+    )
+}
+
+export default StartGameScreen;
